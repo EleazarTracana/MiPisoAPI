7155bf82a69f75b8748b6889b2e80c8f982cf581
eleazartrc <eleazar.tracana@katalystdi.com>
2025-01-24 12:10:04 +0000

    Prepare changes for web scrapper and move modules to simpler classes for now

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 from fastapi import FastAPI
 from fastapi import Depends
-from repositories.house_repository import HouseRepository
+from repositories import WebsitesRepository
 from models.requests.house_request import  HouseRequest
-from models.house import House
-from repositories.database_manager import DatabaseManager, get_database
+from models.responses.house_response import  HouseResponse
+
+from models import House
 from dotenv import load_dotenv 
 
 load_dotenv() 
 
 app = FastAPI()
+
 @app.get('/api/v1/health')
 async def get_health_status():
     return {"Status": "Ok"}
 
-@app.post("/api/v1/houses", response_model=str)
-async def insert_house(data: HouseRequest, house_repository: HouseRepository = Depends()):
-    result = await house_repository.insert(House(price= data.price, website=data.website))
-    return result 
+@app.get("/api/v1/websites/{websiteId}")
+async def get_house_by_id(websiteId: str, websites_repository: WebsitesRepository = Depends()):
+    result = await websites_repository.find_one(websiteId)
+    if result:
+        HouseResponse.from_orm(result)
+    return None
 
-
-
+@app.get("/api/v1/websites/{websiteId}")
+async def get_house_by_id(websiteId: str, websites_repository: WebsitesRepository = Depends()):
+    result = await websites_repository.find_one(websiteId)
+    if result:
+        HouseResponse.from_orm(result)
+    return None
 
 
